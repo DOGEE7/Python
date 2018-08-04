7bb32f98de289ebb319ad4d78d11d98ac3d83cf5
Your Name <you@example.com>
2018-08-04 15:59:28 +0800

    8.2

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,15 +1,16 @@
 import sys
 import pygame
-from settings import  Settings
+from settings import Settings
 
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     # 设置背景色
-    bg_color = (230, 230, 230)
+    # bg_color = (230,230,230,)
     # 开始游戏的主循环
     while True:
         # 监视键盘和鼠标事件
@@ -17,9 +18,10 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
             # 每次循环时都会重绘屏幕
-            screen.fill(bg_color)
+            screen.fill(ai_settings.bg_color)
         # 让最近绘制的屏幕可见
         pygame.display.flip()
 
 
 run_game()
+
